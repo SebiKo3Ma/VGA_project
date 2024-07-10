9bdcbc7a78c0e639f24393289ca4cc9f44bdf235
Sebi Cozma <sebi.sam.cozma@gmail.com>
2024-07-10 15:25:31 +0300

    real time simulation working

--- UART_test_top.v
+++ UART_test_top.v
@@ -1,16 +1,10 @@
 `timescale 1ns/1ns
-module top(input clk, rst, Rx, output[8:0] frame)
-    wire clk_16bd, clk_bd, frame_valid;
-    wire[8:0] temp_frame_ff, temp_frame_nxt;
+module top(input clk, rst, Rx, output[8:0] frame, output frame_valid);
+    wire clk_16bd, clk_bd;
 
-    assign frame = temp_frame_ff;
+    reg[2:0] baud = 2'b01;
+    reg baud_ready = 1'b1;
 
-    clock_handler clock_handler(2'b01, clk, 1'b1, rst, clk_16bd, clk_bd);
-    UART_processor uart_processor(clk_16bd, rst, Rx, 1'b1, 1'b0, 1'b0, 4'b1000, temp_frame_nxt, frame_valid);
-
-    always @(posedge clk, negedge rst) begin
-        if(frame_valid) begin
-            temp_frame_ff <= temp_frame_nxt;
-        end
-    end
+    clock_handler clock_handler(baud, clk, baud_ready, rst, clk_16bd, clk_bd);
+    UART_processor uart_processor(clk_16bd, rst, Rx, 1'b1, 1'b0, 1'b0, 4'b1000, frame, frame_valid);
 endmodule
---
+++ UART_test_top_tb.v
@@ -0,0 +1,33 @@
+`timescale 1ns/1ns
+module tb_top();
+    reg clk, rst, Rx;
+    wire [8:0] frame;
+    wire frame_valid;
+
+    top uart_processor(clk, rst, Rx, frame, frame_valid);
+
+    initial begin
+        clk = 0;
+        forever #5 clk = !clk;
+    end
+
+    initial begin
+        rst = 1'b1;
+        #300000 rst = 1'b0;
+
+        #30000 Rx = 1'b0; //start bit
+
+        #104200 Rx = 1'b1;
+        #104200 Rx = 1'b0;
+        #104200 Rx = 1'b1;
+        #104200 Rx = 1'b0;
+        #104200 Rx = 1'b0;
+        #104200 Rx = 1'b1;
+        #104200 Rx = 1'b1;
+        #104200 Rx = 1'b0;
+
+        #104200 Rx = 1'b0; //parity bit
+        #104200 Rx = 1'b1; //stop bit
+    end
+
+endmodule
