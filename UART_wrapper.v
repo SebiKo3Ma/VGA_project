2073356545f388a02720200e195b142afaf402ec
Sebi Cozma <sebi.sam.cozma@gmail.com>
2024-08-08 16:56:09 +0300

    Untested Uart wrapper + reorganizing

---
+++ UART_wrapper.v
@@ -0,0 +1,8 @@
+module UART_module(input clk_16bd, rst, Rx, valid, input[3:0] data, address, output ack, frame_valid, output[9:0] frame);
+
+    wire parity, parity_type, stop_bits;
+    wire[3:0] frame_length;
+
+    UART_processor UART(.clk_16bd(clk_16bd), .rst(rst), .Rx(Rx), .parity(parity), .parity_type(parity_type), .stop_bits(stop_bits), .frame_length(frame_length), .frame(frame), .frame_valid(frame_valid));
+    uart_regfile uart_regfile(.clk_16bd(clk_16bd), .rst(rst), .valid(valid), .data(data), .address(address), .ack(ack), .parity(parity), .parity_type(parity_type), .stop_bits(stop_bits), .frame_length(frame_length));
+endmodule
