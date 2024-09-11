a42dea516dad831a4071a7d2feb96e6a94aea38d
Sebi Cozma <sebi.sam.cozma@gmail.com>
2024-09-11 14:41:22 +0300

    reorganizing + uart debugger

---
+++ UART_debugger.v
@@ -0,0 +1,22 @@
+`timescale 1ns/1ns
+module UART_debugger(input clk, rst, input[8:0] frame, input frame_valid, output[8:0] debug_frame);
+
+reg[8:0] debug_frame_ff, debug_frame_nxt;
+assign debug_frame = debug_frame_ff;
+
+always @* begin
+    debug_frame_nxt = debug_frame_ff;
+    if(frame_valid) begin
+        debug_frame_nxt = frame;
+    end
+end
+
+always @(posedge clk or posedge rst) begin
+    if(rst) begin
+        debug_frame_ff <= 9'b000000000;
+    end else begin
+        debug_frame_ff <= debug_frame_nxt;
+    end
+end
+
+endmodule
---
+++ tests/debuggers/UART_debugger_tb.v
@@ -0,0 +1,27 @@
+`timescale 1ns/1ns
+module tb_uart_debugger();
+    reg[8:0] frame;
+    reg clk, rst, frame_valid;
+    wire[8:0] debug_frame;
+
+    UART_debugger UART_debugger(.clk(clk), .rst(rst), .frame(frame), .frame_valid(frame_valid), .debug_frame(debug_frame));
+
+    initial begin
+        clk = 0;
+        forever #5 clk = !clk;
+    end
+
+    initial begin
+        rst = 1'b1;
+        #10 rst = 1'b0;
+        #20 frame = 9'b010101010;
+        #2 frame_valid = 1'b1;
+        #10 frame_valid = 1'b0;
+
+        #100 frame = 9'b010110011;
+        #2 frame_valid = 1'b1;
+        #10 frame_valid = 1'b0;
+
+    end
+
+endmodule
