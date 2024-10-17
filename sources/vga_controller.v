9ebdd4bf585591d33610838a4fa2f01206f78081
Sebi Cozma <sebi.sam.cozma@gmail.com>
2024-10-17 12:58:07 +0300

    vga timing fix

--- sources/vga_controller.v
+++ sources/vga_controller.v
@@ -2,53 +2,66 @@
 module vga_controller(input px_clk, rst, input[11:0] px_data, output[10:0] px_h, px_v, 
                         output[3:0] RED, GRN, BLU, output HSYNC, VSYNC);
     reg hs_ff, hs_nxt, vs_ff, vs_nxt;
-    reg[10:0] pxv_ff, pxv_nxt;
     reg[9:0] hcount_ff, hcount_nxt;
-    reg[18:0] vcount_ff, vcount_nxt;
+    reg[9:0] vcount_ff, vcount_nxt;
     
+    localparam h_data = 640;
+    localparam h_fp = 16;
+    localparam h_pw = 96;
+    localparam h_bp = 48;
+    localparam h_total = 800;
+
+    localparam v_data = 480;
+    localparam v_fp = 10;
+    localparam v_pw = 2;
+    localparam v_bp = 29;
+    localparam v_total = 521;
+
+    localparam polarity = 1;
+
     assign RED = px_data[11:8];
     assign GRN = px_data[7:4];
     assign BLU = px_data[3:0];
     assign HSYNC = hs_ff;
     assign VSYNC = vs_ff;
 
-    assign px_h = hcount_ff - 144;
-    assign px_v = pxv_ff - 31;
+    assign px_h = hcount_ff;
+    assign px_v = vcount_ff;
 
     always @* begin
         hs_nxt = hs_ff;
         vs_nxt = vs_ff;
-        pxv_nxt = pxv_ff;
         hcount_nxt = hcount_ff + 1;
-        vcount_nxt = vcount_ff + 1;
+        vcount_nxt = vcount_ff;
 
-        //heorizontal zero detect
-        if(hcount_ff == 10'd799) begin
+        //horizontal zero detect
+        if(hcount_ff == h_total - 1) begin
             hcount_nxt = 10'd0;
-            hs_nxt = 1'b1;
-            pxv_nxt = pxv_ff + 1;
+            vcount_nxt = vcount_ff + 1;
         end
 
-        //horizontal 3.84us detect
-        if(hcount_ff == 10'd95) begin
-            hs_nxt = 1'b0;
+        //horizontal sync start
+        if(hcount_ff == h_data + h_fp - 1) begin
+            hs_nxt = !polarity;
+        end
+        //horizontal sync stop
+        if(hcount_ff == h_data + h_fp + h_pw - 1) begin
+            hs_nxt = polarity;
         end
 
         //vertical zero detect
-        if(vcount_ff == 19'd416799) begin
+        if(vcount_ff == v_total - 1) begin
             vcount_nxt = 10'd0;
-            vs_nxt = 1'b1;
-            pxv_nxt = 1'b0;
         end
 
-        //vertical 64us detect
-        if(vcount_ff == 19'd1599) begin
-            vs_nxt = 1'b0;
+        //vertical sync start
+        if(vcount_ff == v_data + v_fp - 1) begin
+            vs_nxt = !polarity;
         end
-
-
-
-
+        //vertical sync stop
+        if(vcount_ff == v_data + v_fp + v_pw - 1) begin
+            vs_nxt = polarity;
+        end
     end
 
     always @(posedge px_clk or posedge rst) begin
@@ -56,15 +69,12 @@ module vga_controller(input px_clk, rst, input[11:0] px_data, output[10:0] px_h,
             hs_ff <= 1'b1;
             vs_ff <= 1'b1;
             hcount_ff <= 10'd0;
-            vcount_ff <= 19'd0;
-            pxv_ff <= 11'd0;
+            vcount_ff <= 10'd0;
         end else begin
             hs_ff <= hs_nxt;
             vs_ff <= vs_nxt;
-            pxv_ff <= pxv_nxt;
             hcount_ff <= hcount_nxt;
             vcount_ff <= vcount_nxt;
         end
-
     end
 endmodule
